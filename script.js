a8293b45c37695ee31305ed7d23dd5971aa18ae7
GeekCentric <ts22082@gmail.com>
2019-06-03 11:36:55 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let weatherAPIKey = '5da0c3a42a26467ed286a2264be820ff'
+let giphyAPIKey = 'CB3u9m9EcYrw3EnygcS0FX4P91gsyN8N'
+
+let weatherURL =
+  'https://api.openweathermap.org/data/2.5/weather?zip=95616&appid=' +
+  weatherAPIKey +
+  '&units=imperial'
+
+let giphyURL =
+  'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=' +
+  giphyAPIKey +
+  '&limit=5'
+
+$.ajax({
+  url: weatherURL,
+  method: 'GET'
+}).then(function(response) {
+  console.log(response)
+})
+
+$.ajax({
+  url: giphyURL,
+  method: 'GET'
+}).then(function(response) {
+  console.log(response)
+})
+
+$('#weatherSubmit').click(function() {
+  weatherZip = $('#weatherSearch').val()
+  let weatherURL =
+    'https://api.openweathermap.org/data/2.5/weather?zip=' +
+    weatherZip +
+    '&appid=5da0c3a42a26467ed286a2264be820ff' +
+    '&units=imperial'
+
+  $.ajax({
+    url: weatherURL,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response)
+    $('#name').html(response.name)
+    $('#temp').html(response.main.temp)
+    $('#wind').html(response.wind.speed)
+  })
+})
